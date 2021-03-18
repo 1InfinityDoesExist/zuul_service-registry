fd5114247b5f0ff61d6d2f7a1c451a742250a431
1InfinityDoesExist <patelavinash508@gmail.com>
2021-03-18 22:40:30 +0545

    hystrix-dashboard added

--- zuul-api-gateway/src/main/java/in/zuul/apigateway/ApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/in/zuul/apigateway/ApiGatewayApplication.java
@@ -3,13 +3,15 @@ package in.zuul.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
 public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
 
 }
