71c86aa732c5f6443708b1c7678ec4e4446b9a8f
Avinash Patel <avinash.patel@gaiansolutions.com>
2021-03-21 01:23:23 +0530

    refactor

---
+++ department-service/src/main/java/zuularch/department/service/filter/IamCorsFilter.java
@@ -0,0 +1,61 @@
+package zuularch.department.service.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * created by gaian on 31/10/19
+ **/
+
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class IamCorsFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain)
+        throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpServletRequest request = (HttpServletRequest) req;
+        String authorization = request.getHeader("Authorization");
+        String tokenFromHeader = request.getHeader("token");
+        String uri = request.getRequestURI();
+        log.info("::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+        log.info(authorization);
+        log.info(tokenFromHeader);
+        log.info(uri);
+        log.info("::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+            "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN,Access-Control-Allow-Origin, X-Auth-Token");
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
