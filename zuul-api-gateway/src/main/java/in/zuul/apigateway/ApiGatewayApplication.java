041e51cb9fc60957c891374dbf10246813d87852
Avinash Patel <avinash.patel@gaiansolutions.com>
2021-03-31 11:28:30 +0530

    backup

--- zuul-api-gateway/src/main/java/in/zuul/apigateway/ApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/in/zuul/apigateway/ApiGatewayApplication.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaClient // if you use discovery client it will not appear in eureka dashboard
+@EnableEurekaClient
 @SpringBootApplication
 @EnableHystrix
-@EnableZuulProxy
+// @EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
 
 }
